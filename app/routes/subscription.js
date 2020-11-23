7e288d5ad14286164dc47f56522b6c4ed078a2be
Alex Iskakov <ma3ca3@gmail.com>
2020-11-23 20:20:43 +0500

    срусл

--- app/routes/subscription.js
+++ app/routes/subscription.js
@@ -3,13 +3,11 @@ import Route from '@ember/routing/route';
 export default class SubscriptionRoute extends Route {
    init() {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    let res = fetch('https://www.gicmoc.ru/subscription/?id=ff', {
+    let res = fetch('https://gicmoc.ru/subscription/?id=ff', {
       mode: "cors",
-      headers:{
-        "Access-Control-Allow-Origin": "*"
-      }
+      
     });
-   res.then((res1) => {console.log(res1); let f =  res1.text(); f.then((res) => {console.log(res)})}).catch((mes) => {console.log(mes)});
+   res.then((res1) => {console.log(res1); let f =  res1.json(); f.then((res) => {console.log(res)})}).catch((mes) => {console.log(mes)});
 
   }
 }
